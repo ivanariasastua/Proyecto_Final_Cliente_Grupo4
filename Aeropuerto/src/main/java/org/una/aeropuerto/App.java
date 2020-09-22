a7d3bf5a5c386ddcaa436381d870082986ca0fd3
ivanariasastua <69368731+ivanariasastua@users.noreply.github.com>
2020-09-21 21:28:56 -0600

    Inicio del Proyecto

---
+++ Aeropuerto/src/main/java/org/una/aeropuerto/App.java
@@ -0,0 +1,38 @@
+package org.una.aeropuerto;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import javafx.stage.StageStyle;
+import org.una.aeropuerto.util.FlowController;
+
+/**
+ * JavaFX App
+ */
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        FlowController.getInstance().goViewInNoResizableWindow("LogIn", Boolean.TRUE, StageStyle.UNDECORATED, 540, 435);
+    }
+/*
+    static void setRoot(String fxml) throws IOException {
+        scene.setRoot(loadFXML(fxml));
+    }
+
+    private static Parent loadFXML(String fxml) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml + ".fxml"));
+        return fxmlLoader.load();
+    }
+*/
+    public static void main(String[] args) {
+        launch();
+    }
+
+}
---
+++ Aeropuerto/src/main/java/org/una/aeropuerto/controller/LogInController.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuerto.controller;
+
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.input.MouseEvent;
+
+/**
+ * FXML Controller class
+ *
+ * @author ivana
+ */
+public class LogInController extends Controller implements Initializable {
+
+    @FXML
+    private JFXTextField txtUserName;
+    @FXML
+    private JFXPasswordField txtPassword;
+
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+
+    @Override
+    public void initialize() {
+    }
+
+    @Override
+    public void adjustWidth(double witdh) {
+    }
+
+    @Override
+    public void adjustHeigth(double height) {
+    }
+
+    @FXML
+    private void accionLogIn(ActionEvent event) {
+    }
+
+    @FXML
+    private void accionCerrar(ActionEvent event) {
+        this.getStage().close();
+    }
+
+    @FXML
+    private void accionMinimizar(MouseEvent event) {
+        this.minimizeWindow();
+    }
+    
+}
