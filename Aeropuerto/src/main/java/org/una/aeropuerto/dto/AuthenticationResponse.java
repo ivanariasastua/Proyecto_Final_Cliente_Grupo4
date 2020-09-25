c621dc62966ad895f71c257c0f4ef889b9f933ed
AngieCordoba <cordobaangie98@gmail.com>
2020-09-24 20:31:53 -0600

    LogIn

---
+++ Aeropuerto/src/main/java/org/una/aeropuerto/dto/AuthenticationResponse.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuerto.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+/**
+ *
+ * @author ivana
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class AuthenticationResponse {
+
+    private String jwt;
+    private EmpleadosDTO usuario;
+    
+
+}
---
+++ Aeropuerto/src/main/java/org/una/aeropuerto/service/AuthenticationService.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.una.aeropuerto.service;
+
+import org.una.aeropuerto.dto.AuthenticationRequest;
+import org.una.aeropuerto.dto.AuthenticationResponse;
+import org.una.aeropuerto.util.AppContext;
+import org.una.aeropuerto.util.Request;
+import org.una.aeropuerto.util.Respuesta;
+
+/**
+ *
+ * @author cordo
+ */
+public class AuthenticationService {
+    
+    public Respuesta LogIn(String userName, String userPassword){
+        try{
+            AuthenticationRequest authetication = new AuthenticationRequest(userName, userPassword);
+            Request request = new Request("authentication/login");
+            request.post(authetication);
+            if(request.isError()){
+                return new Respuesta(false, request.getError(), "Iniciar Sesion: "+request.getMensajeRespuesta());
+            }
+            AuthenticationResponse usuario = (AuthenticationResponse) request.readEntity(AuthenticationResponse.class);
+            AppContext.getInstance().set("UsuarioAutenticado", usuario);
+            AppContext.getInstance().set("token", "bearer " + usuario.getJwt());
+            return new Respuesta(true, "Usuario", usuario);
+        }catch(Exception ex){
+            return new Respuesta(false, ex.toString(), "Iniciar Sesion: Error al comunicarse con el servidor");
+        }
+    }
+    
+
+}
